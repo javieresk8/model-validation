34f36dfb4f12ded91d59aa2924ff0166c1f2d0a7
javiererazo <alex.erazo01@epn.edu.ec>
2024-02-15 16:35:01 -0500

    initial commit

---
+++ ModelValidationExample/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidationExample.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ModelValidationExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("Register")]
+        public IActionResult Index(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errorList = string.Join("\n", ModelState.Values
+                    .SelectMany(value => value.Errors)
+                    .Select(err => err.ErrorMessage));
+
+                return BadRequest(errorList);
+            }
+            return Content($"{person}");
+        }
+    }
+}
+
---
+++ ModelValidationExample/Models/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidationExample.Models
+{
+	public class Person
+	{
+        [Required(ErrorMessage = " {0} es obligatorio")]
+        [StringLength(40, MinimumLength = 3,
+            ErrorMessage = "{0} debe tener minimo {2} y maximo {1} caracteres")]
+        [RegularExpression("^[A-Za-z .]$",
+            ErrorMessage = "{0} solo puede tener caracters alfabeticos, " +
+            "espacios y numeros")]
+        [Display(Name = "Nombre persona")]
+        public string? PersonName { get; set; }
+
+        [EmailAddress(ErrorMessage = "{0} debe ser valido")]
+        public string? Email { get; set; }
+
+        [EmailAddress(ErrorMessage = "{0} debe ser valido para el telefono")]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "{0} no puede estar en blanco")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "{0} no puede estar en blanco")]
+        [Compare("Password", ErrorMessage = "{0} no puede ser diferente a {1}")]
+        [Display(Name ="Re enter password")]
+        public string? ConfirmPassword { get; set; }
+
+        [Range(0, 99.99, ErrorMessage = "{0} es minimo {1} y maximo {2}")]
+        public double? Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person object {PersonName} - {Email} {Phone} {Password} {ConfirmPassword} {Price}";
+        }
+
+    }
+}
+
